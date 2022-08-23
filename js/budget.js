921ea71ca6ae36754a9dd548a41d6e7479f68fe3
opuzayed <zayedopu4933@gmail.com>
2022-08-23 22:09:26 +0600

    input error handling done

--- js/budget.js
+++ js/budget.js
@@ -1,15 +1,38 @@
-document.getElementById('btn-calculate').addEventListener('click', function(){
-    const perPlayerBudget = getInputFieldValueById('budget-field');
-    const playerTotalExpenses = perPlayerBudget * 5;
-    setElementValueById('player-cost-element', playerTotalExpenses);
+let playerTotalExpenses;
 
-    document.getElementById('btn-total').addEventListener('click', function(){
-        const managerEpenses = getInputFieldValueById('manager-inputfield');
-        const coachExpenses = getInputFieldValueById('coach-inputfield');
-    
-        const totalExpenses = playerTotalExpenses + managerEpenses + coachExpenses;
-        setElementValueById('total-inputfield', totalExpenses);
-    });
+document.getElementById("btn-calculate").addEventListener("click", function () {
+  const perPlayerBudget = getInputFieldValueById("budget-field");
+  if (
+    perPlayerBudget > 0 &&
+    perPlayerBudget != null &&
+    typeof perPlayerBudget == "number"
+  ) {
+    playerTotalExpenses = perPlayerBudget * 5;
+    setElementValueById("player-cost-element", playerTotalExpenses);
+  } else {
+    alert("Please Input Per Player Value In Integer");
+  }
 });
 
- 
+document.getElementById("btn-total").addEventListener("click", function () {
+  if (playerTotalExpenses > 0 && typeof playerTotalExpenses == "number") {
+    const managerEpenses = getInputFieldValueById("manager-inputfield");
+    const coachExpenses = getInputFieldValueById("coach-inputfield");
+    if (
+      managerEpenses > 0 &&
+      managerEpenses != null &&
+      typeof managerEpenses == "number" &&
+      coachExpenses > 0 &&
+      coachExpenses != null &&
+      typeof coachExpenses == "number"
+    ) {
+      const totalExpenses =
+        playerTotalExpenses + managerEpenses + coachExpenses;
+      setElementValueById("total-inputfield", totalExpenses);
+    } else {
+      alert("Please Input Valid Number");
+    }
+  } else {
+    alert("Expenses Not Calculated");
+  }
+});
--- js/select.js
+++ js/select.js
@@ -1,22 +1,22 @@
 const player = [];
 document.querySelectorAll(".select-player").forEach(selectPlayer);
-function selectPlayer(item) {
-  item.addEventListener("click", function () {
+function selectPlayer(item) 
+{
+  item.addEventListener("click", function () 
+  {
     const playerName = this.parentNode.querySelector("h5").innerText;
-    if (player.length < 5) {
+    if (player.length < 5) 
+    {
       player.push(playerName);
-      //   document.getElementById("selectedPlayers").innerText = player;
-
       let item = document.createElement("li");
       item.innerText = playerName;
       document.getElementById("selectedPlayers").appendChild(item);
-
       this.classList.add("disable");
-    } else {
+    } 
+    else 
+    {
       alert("Cannot Select More Than 5");
     }
-
-    console.log(player);
   });
 }
 
